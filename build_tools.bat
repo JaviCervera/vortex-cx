b522c5a74605e8d1aab5d6ea790f4a33277e6fef
Javier San Juan Cervera <jedive@gmail.com>
2017-03-28 16:10:07 +0200

    Added color requester in Win32 (it freezes the app, must check why...)

--- build_tools.bat
+++ build_tools.bat
@@ -3,11 +3,14 @@
 echo building meshtool...
 g++ -std=c++03 -w -O2 -m32 -o meshtool.data/meshtool.exe src_meshtool/meshtool.cc  src_meshtool/loadmesh.cc src_meshtool/genxml.cc -Lsrc_meshtool/irrlicht -lirrlicht.win32 -lgdi32
 
+echo building fltk color requester...
+g++ -std=c++03 -O2 -m32 -o meshtool.data/fltkrequestcolor.exe -Isrc_meshtool/fltk src_meshtool/fltkrequestcolor.cc -Lsrc_meshtool/fltk -lfltk.win32 -lcomctl32 -lgdi32 -lole32 -luuid
+
 rem echo building loadmesh dll...
 rem g++ -std=c++03 -w -O2 -m32 -shared -o meshtool.data/loadmesh.dll -DBUILD_SHARED src_meshtool/loadmesh.cc -Lsrc_meshtool/irrlicht -lirrlicht.win32 -lgdi32
 
-echo building fonttool...
-g++ -std=c++03 -O2 -m32 -o fonttool.exe src.fonttool/stb_image_write.c src.fonttool/stb_truetype.c src.fonttool/main.cpp
+rem echo building fonttool...
+rem g++ -std=c++03 -O2 -m32 -o fonttool.exe src.fonttool/stb_image_write.c src.fonttool/stb_truetype.c src.fonttool/main.cpp
 
 echo done.
 pause
