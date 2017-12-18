027edc65f64a9b3452497db31d5d88a0036b10b2
Javier San Juan Cervera <jedive@gmail.com>
2017-12-18 14:29:42 +0100

    - Removed XML support in fonttool - Allowed font textures to have different width and height (this should save texture space) - Added size suffix to fonts in fonttool - Added x offset to fonts

--- src_tools/fonttool.cc
+++ src_tools/fonttool.cc
@@ -3,8 +3,9 @@
 #define STB_TRUETYPE_IMPLEMENTATION
 #include "stb_truetype.h"
 #include "stringutils.h"
+#include <fstream>
 #include <iostream>
-#include <stdlib.h>
+#include <vector>
 
 using namespace std;
 
@@ -20,82 +21,75 @@ int main(int argc, char* argv[]) {
   int fontsize = NumberFromString<int>(argv[2]);
 
   // read file
-  FILE* handle = fopen(fontfile.c_str(), "rb");
-  if ( !handle ) {
+  ifstream file(fontfile.c_str(), std::ios::binary | std::ios::ate);
+  if ( !file.is_open() ) {
     cout << "Error: Could not load file '" << fontfile << "'" << endl;
     return -1;
   }
-  fseek(handle, 0, SEEK_END);
-  long size = ftell(handle);
-  fseek(handle, 0, SEEK_SET);
-  unsigned char* buf = (unsigned char*)malloc(size);
-  fread(buf, sizeof(unsigned char), size, handle);
-  fclose(handle);
+  std::vector<unsigned char> buf(file.tellg());
+  file.seekg(0);
+  file.read(reinterpret_cast<char*>(&buf[0]), buf.size());
 
   // bake font into temp alpha buffer
   stbtt_bakedchar charData[224];
-  bool imgPacked = false;
-  int imgsize = 32;
-  unsigned char* alphaBuffer = NULL;
-  while ( !imgPacked ) {
-    alphaBuffer = (unsigned char*)malloc(imgsize * imgsize);
-    if ( stbtt_BakeFontBitmap(buf, 0, fontsize, alphaBuffer, imgsize, imgsize, 32, 224, charData) > 0 ) {
-      imgPacked = true;
-    } else {
-      free(alphaBuffer);
-      imgsize *= 2;
-    }
+  int imgWidth = 128, imgHeight = 128;
+  std::vector<unsigned char> alphaBuffer(imgWidth * imgHeight);
+  while ( stbtt_BakeFontBitmap(&buf[0], 0, fontsize, &alphaBuffer[0], imgWidth, imgHeight, 32, 224, charData) <= 0 ) {
+    if ( imgWidth == imgHeight ) imgWidth *= 2;
+    else imgHeight *= 2;
+    alphaBuffer.resize(imgWidth * imgHeight);
   }
-  free(buf);
   
   // copy alpha buffer into color buffer
-  unsigned char* colorBuffer = (unsigned char*)malloc(imgsize * imgsize * 4);
-  for ( int i = 0; i < imgsize*imgsize; i++ ) {
-    colorBuffer[i*4 + 0] = 255;
-    colorBuffer[i*4 + 1] = 255;
-    colorBuffer[i*4 + 2] = 255;
+  std::vector<unsigned char> colorBuffer(imgWidth * imgHeight * 4, 255);
+  for ( int i = 0; i < imgWidth*imgHeight; i++ ) {
     colorBuffer[i*4 + 3] = alphaBuffer[i];
   }
-  free(alphaBuffer);
   
   // write image to a png file
-  stbi_write_png((StripExt(fontfile) + ".png").c_str(), imgsize, imgsize, 4, colorBuffer, 0);
-  free(colorBuffer);
+  string texName = StripExt(fontfile) + "_" + StringFromNumber(fontsize) + ".png";
+  stbi_write_png(texName.c_str(), imgWidth, imgHeight, 4, &colorBuffer[0], 0);
 
-  // generate xml description of font
-  string fnt = "<font>\n";
-  fnt += "\t<image>" + StripExt(fontfile) + ".png" + "</image>\n";
-  /*fnt += "\t<alphas>";
-  for ( int i = 0; i < imgsize*imgsize; ++i ) {
-    fnt += StringFromNumber((int)alphaBuffer[i]);
-    if ( i < imgsize*imgsize - 1) fnt += ",";
-  }
-  fnt += "</alphas>\n";*/
-  fnt += "\t<buffer_width>" + StringFromNumber(imgsize) + "</buffer_width>\n";
-  fnt += "\t<font_size>" + StringFromNumber(fontsize) + "</font_size>\n";
-  fnt += "\t<glyphs>\n";
-  for ( unsigned int i = 0; i < 224; i++ ) {
+  // write glyph data to fnt.dat file
+  ofstream dat((StripExt(fontfile) + "_" + StringFromNumber(fontsize) + ".fnt.dat").c_str(), std::ios::binary | std::ios::trunc);
+
+  // id & version
+  string id = "FN01";
+  dat.write(id.c_str(), id.size());
+
+  // texture name
+  texName = StripPath(texName);
+  int texLen = static_cast<int>(texName.size());
+  dat.write(reinterpret_cast<const char*>(&texLen), sizeof(texLen));
+  dat.write(reinterpret_cast<const char*>(texName.c_str()), texLen);
+
+  // font height
+  unsigned short fontHeight = static_cast<unsigned short>(fontsize);
+  dat.write(reinterpret_cast<const char*>(&fontHeight), sizeof(fontHeight));
+
+  // num glyphs
+  int numGlyphs = 224;
+  dat.write(reinterpret_cast<const char*>(&numGlyphs), sizeof(numGlyphs));
+
+  // first char
+  int firstChar = 32;
+  dat.write(reinterpret_cast<const char*>(&firstChar), sizeof(firstChar));
+
+  // glyphs
+  for ( size_t i = 0; i < numGlyphs; ++i ) {
+    float x = charData[i].x0;
+    float y = charData[i].y0;
     float width = charData[i].x1 - charData[i].x0;
     float height = charData[i].y1 - charData[i].y0;
-    float x = charData[i].x0;
-    float y = charData[i].y0; //imgsize - charData[i].y0 - height;
     float xoffset = charData[i].xoff;
     float yoffset = height + charData[i].yoff;
-    fnt += "\t\t<glyph ";
-    fnt += "x=\"" + StringFromNumber(x) + "\" ";
-    fnt += "y=\"" + StringFromNumber(y) + "\" ";
-    fnt += "width=\"" + StringFromNumber(width) + "\" ";
-    fnt += "height=\"" + StringFromNumber(height) + "\" ";
-    fnt += "xoffset=\"" + StringFromNumber(xoffset) + "\" ";
-    fnt += "yoffset=\"" + StringFromNumber(yoffset) + "\" ";
-    fnt += "/>\n";
+    dat.write(reinterpret_cast<const char*>(&x), sizeof(x));
+    dat.write(reinterpret_cast<const char*>(&y), sizeof(y));
+    dat.write(reinterpret_cast<const char*>(&width), sizeof(width));
+    dat.write(reinterpret_cast<const char*>(&height), sizeof(height));
+    dat.write(reinterpret_cast<const char*>(&xoffset), sizeof(xoffset));
+    dat.write(reinterpret_cast<const char*>(&yoffset), sizeof(yoffset));
   }
-  fnt += "\t</glyphs>\n";
-  fnt += "</font>";
-  
-  // write the font to a file
-  WriteString(fnt, StripExt(fontfile) + ".fnt.xml", false);
-  //cout << fnt << endl;
 
   return 0;
 }
